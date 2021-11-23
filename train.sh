2042fbf15ca005e178e22173bea33ab965a6c52c
Soheil Changizi <cocolico12@gmail.com>
2021-11-23 05:39:42 -0600

    changed train.sh

--- train.sh
+++ train.sh
@@ -5,13 +5,13 @@
 # test_data: by default, points to the validation set, since this is the set that
 #   will be evaluated after each training iteration. If you wish to test
 #   on the final (held-out) test set, change 'val' to 'test'.
-type=java-large-model
-dataset_name=java-large
-data_dir=data/java-large
+type=java-med-model
+dataset_name=java-med
+data_dir=/content/drive/MyDrive/java-med
 data=${data_dir}/${dataset_name}
 test_data=${data_dir}/${dataset_name}.val.c2s
 model_dir=models/${type}
 
 mkdir -p ${model_dir}
 set -e
-python3 -u code2seq.py --data ${data} --test ${test_data} --save_prefix ${model_dir}/model
+python3 -u /content/code2seq/code2seq.py --data ${data} --test ${test_data} --save_prefix ${model_dir}/model
